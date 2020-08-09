17f95d9882c3d3fab5945c86513fbbc464afc564
duy cuong <vuduycuong6789@gmail.com>
2020-08-09 17:30:29 +0700

    reset password

--- Modules/Admin/Routes/web.php
+++ Modules/Admin/Routes/web.php
@@ -7,15 +7,25 @@ use Illuminate\Support\Facades\Route;
 | Admin Auth Routes
 |--------------------------------------------------------------------------
 */
-Route::group(['prefix' => 'admin', 'namespace' => 'Auth'], function () {
-    Route::get('login', 'LoginController@showLoginForm');
-    Route::post('login', 'LoginController@login')->name('admin.login');
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Auth',
+    'as' => 'admin.',
+], function () {
+    Route::get('login', 'LoginController@showLoginForm')->name('formLogin');
+    Route::post('login', 'LoginController@login')->name('login');
     Route::post('logout', 'LoginController@logout')->name('admin.logout');
+    Route::get('forgot-password', 'ForgotPasswordController@showLinkRequestForm')->name('password.reset.showForm');
+    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('send.link.email');
+    Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.showResetForm');
+    Route::post('password/reset', 'ResetPasswordController@reset')->name('password.update');
+
 });
 
 Route::group([
     'prefix' => 'admin',
     'as' => 'admin.',
+    'middleware' => 'Assign.guard:admin',
 ], function () {
     Route::get('/', 'DashboardController@index')->name('dashboard');
     Route::resource('khach-hang', 'CustomerController');
