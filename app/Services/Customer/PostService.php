62b5e4f574899bc08dc26d7e796849bb19cbaf69
duy cuong <vuduycuong6789@gmail.com>
2020-08-21 22:17:52 +0700

    post

---
+++ app/Services/Customer/PostService.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Services\Customer;
+
+use App\Models\BranchSalon;
+use App\Models\Order;
+use App\Models\OrderService;
+use App\Models\Service;
+use App\Models\TimeSchedule;
+use App\Traits\WebResponseTrait;
+
+class PostService
+{
+    use WebResponseTrait;
+
+    public function listPost()
+    {
+        
+    }
+
+}
