0f161b39cb0f197034aed134c4f15a4a1cfee9b6
Tran Hong Phuoc <phuocth@vnext.com.vn>
2020-08-24 21:16:17 +0700

    thong ke

--- app/Services/Admin/StatisticService.php
+++ app/Services/Admin/StatisticService.php
@@ -5,6 +5,7 @@ use App\Models\Customer;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Arr;
 use App\Mail\MailCreateCustomer;
+use App\Models\Order;
 use Carbon\Carbon;
 use Hash;
 use Illuminate\Support\Str;
@@ -13,8 +14,20 @@ class StatisticService
 {
     public function customer()
     {
+        $countCustomer = Customer::all()->count();
+        $now = Carbon::now('Asia/Ho_Chi_Minh');
+        $countCustomerMonth = Customer::
+            whereMonth('created_at', $now->month)
+            ->whereYear('created_at', $now->year)
+            ->count();
+        // $familiarCustomers = Order:: ;
+
         return view(
             'admin::statistic.index',
+            [
+                'countCustomer' => $countCustomer,
+                'countCustomerMonth' => $countCustomerMonth,
+            ]
         );
     }
 
